e518c75864dfd9673e5dd43152d11b02e199b742
Ian Gilbert <ian.d.gilbert@gmail.com>
2020-05-08 02:39:27 -0700

    Accumulation of way too many random changes I've made over the last over the last year or so. This is the current working version, with 2-player and minimax implementation, and I'll fork the project to continue development of MCTS.

--- src/minimax.py
+++ src/minimax.py
@@ -1,9 +1,3 @@
-from math import inf
-from random import choice
-from copy import deepcopy
-from boardclasses import TicTacToeBoard
-
-
 """
 This is a modified version of the minimax algorithm for Tic Tac Toe from the following source:
 Link: https://github.com/Cledersonbc/tic-tac-toe-minimax
@@ -16,7 +10,18 @@ License: GNU GENERAL PUBLIC LICENSE (GPL)
 """
 
 
-def heuristic(state, depth):
+from typing import Tuple, List, Optional
+from math import inf
+from random import choice
+from copy import deepcopy
+from src.boardclasses import TicTacToeBoard, LocalBoard, GlobalBoard
+
+
+COMP: int
+HUMAN: int
+
+
+def heuristic(state: TicTacToeBoard, depth: int) -> int:
     """
     Heuristic evaluation of the current board state
     :param state: the current board state
@@ -31,7 +36,7 @@ def heuristic(state, depth):
     return score
 
 
-def get_empty_cells(state):
+def get_empty_cells(state: TicTacToeBoard) -> List[Tuple[int, int]]:
     """
     Returns the coordinates of all the unclaimed spaces on the board
     :param state: the current board state
@@ -41,11 +46,11 @@ def get_empty_cells(state):
     for row_index, row in enumerate(state.board):
         for col_index, cell in enumerate(row):
             if cell == 0:
-                cells.append([row_index, col_index])
+                cells.append((row_index, col_index))
     return cells
 
 
-def minimax(state, depth, player):
+def minimax(state: TicTacToeBoard, depth: int, player: int) -> List[int]:
     """
     The minimax algorithm itself. Returns a random move if the depth is 9, otherwise the first move would always be the
     top left corner.
@@ -58,7 +63,7 @@ def minimax(state, depth, player):
     if depth == 9:
         row = choice([0, 1, 2])
         col = choice([0, 1, 2])
-        return row, col, ''
+        return [row, col, 0]
 
     if player == COMP:
         best = [-1, -1, -inf]
@@ -69,24 +74,25 @@ def minimax(state, depth, player):
         score = heuristic(state, depth)
         return [-1, -1, score]
 
-    for cell in get_empty_cells(state):
-        row, col = cell[0], cell[1]
-        state.board[row][col] = player
-        score = minimax(state, depth - 1, (player % 2) + 1)
-        state.board[row][col] = 0
-        score[0], score[1] = row, col
+    for row, col in get_empty_cells(state):  # iterate through list of available moves
+        state.board[row][col] = player  # mark current available move for current player
+        result = minimax(state, depth - 1, (player % 2) + 1)  # search through branch until it terminates
+        state.board[row][col] = 0  # undo move for future iterations
+        result[0], result[1] = row, col  # set result to current move
 
+        # If COMP move is better than previous best move, make this the new best
         if player == COMP:
-            if score[2] > best[2]:
-                best = score
+            if result[2] > best[2]:
+                best = result
+        # Same for HUMAN move, but reversed. We want most negative move, because that is good for the human
         else:
-            if score[2] < best[2]:
-                best = score
+            if result[2] < best[2]:
+                best = result
 
     return best
 
 
-def bot_turn(global_board, bot):
+def bot_turn(global_board: GlobalBoard, bot: int):
     """
     Finds the next local board to play on. If undefined, it uses the minimax algorithm to decide which board to play on.
     Then uses the minimax algorithm again to decide where to play on that board.
@@ -99,23 +105,26 @@ def bot_turn(global_board, bot):
     global HUMAN
     COMP = bot
     HUMAN = (bot % 2) + 1
+    local_board: Optional[LocalBoard] = None
 
     # If the next board is undetermined
-    if all(lb.focus == lb.playable for lb in global_board.local_boards):
+    if all(lb.focus == lb.playable for lb in global_board.local_board_list):
         # Use minimax on the global board to determine the next local board
         depth = len(get_empty_cells(global_board))
         state = TicTacToeBoard()
         state.board = deepcopy(global_board.board)
         row, col, _ = minimax(state, depth, COMP)
 
-        local_board = global_board.local_boards[row * 3 + col]
+        local_board = global_board.local_board_list[row * 3 + col]
 
     else:
         # Find the local board in focus
-        for lb in global_board.local_boards:
+        for lb in global_board.local_board_list:
             if lb.focus:
                 local_board = lb
                 break
+
+    assert local_board is not None
 
     # local_board now defined. Now use minimax to find row and col of next move
     depth = len(get_empty_cells(local_board))
--- src/ultimate_tic_tac_toe.py
+++ src/ultimate_tic_tac_toe.py
@@ -1,68 +1,106 @@
+from typing import Optional  # also Tuple from src.gui.pyg_init
 import random as rand
 import sys
-from boardclasses import GlobalBoard
-import minimax
-from gui.pyg_init import *  # contains pygame import
-from gui import pyg_util
+from src.boardclasses import GlobalBoard, LocalBoard
+from src import minimax
+from src.gui.pyg_init import *  # contains pygame import
+from src.gui import pyg_util
+
+
+"""Game Setup Constants"""
+
+# Opponent setting
+TWO_PLAYER = "2 Player"
+BEGINNER = "Beginner"
+MCTS = "MCTS"
+FOO1 = "Foo 1"
+FOO2 = "Foo 2"
+FOO3 = "Foo 3"
+
+# Turn order
+RANDOM_ORDER = "Random"
+PLAYER_FIRST = "First"
+PLAYER_SECOND = "Second"
+
 
 """PyGame Initialization"""
 pygame.init()
 
-# define the main screen
-screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+# Define the main screen
+screen: pygame.display = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption("Ultimate Tic Tac Toe")
 
 rules = pyg_util.RulesScreen()
 
 # menu items
-textarea = pyg_util.TextArea()
+textarea = pyg_util.TextArea()  # Displays who's turn it is and game outcome
 
-rulesbutton = pyg_util.Button((GLOBALBOARDSIZE + BOARDERSIZE, BOARDERSIZE + int(1.75 * SQUARESIZE),
-                               LOCALBOARDSIZE, SQUARESIZE), 'Show Rules', colorfamily=BLUE_FAMILY, textcolor=LIGHT_GRAY)
-newgamebutton = pyg_util.Button((GLOBALBOARDSIZE + BOARDERSIZE, SCREENHEIGHT - BOARDERSIZE - int(2.75 * SQUARESIZE),
-                                 LOCALBOARDSIZE, SQUARESIZE), 'New Game', colorfamily=GREEN_FAMILY)
-quitbutton = pyg_util.Button((GLOBALBOARDSIZE + BOARDERSIZE, SCREENHEIGHT - BOARDERSIZE - int(1.25 * SQUARESIZE),
-                              LOCALBOARDSIZE, SQUARESIZE), 'Quit Game', colorfamily=RED_FAMILY)
-alg_options = pyg_util.GameOptions((GLOBALBOARDSIZE + BOARDERSIZE, LOCALBOARDSIZE + WHITESPACE), 'Very Easy',
-                                   '2 Player', 'Very Easy')
-order_options = pyg_util.GameOptions(
-    (GLOBALBOARDSIZE + BOARDERSIZE + int(.55 * LOCALBOARDSIZE), LOCALBOARDSIZE + WHITESPACE), 'Random', 'Random',
-    'First', 'Second')
+rulesbutton = pyg_util.Button(  # Show game rules
+    (GLOBALBOARDSIZE + BOARDERSIZE, BOARDERSIZE + int(1.75 * SQUARESIZE)),
+    'Show Rules',
+    colorfamily=BLUE_FAMILY,
+    textcolor=LIGHT_GRAY
+)
+
+newgamebutton = pyg_util.Button(  # Start new game
+    (GLOBALBOARDSIZE + BOARDERSIZE, SCREENHEIGHT - BOARDERSIZE - int(2.75 * SQUARESIZE)),
+    'New Game',
+    colorfamily=GREEN_FAMILY
+)
+
+quitbutton = pyg_util.Button(  # Quit game
+    (GLOBALBOARDSIZE + BOARDERSIZE, SCREENHEIGHT - BOARDERSIZE - int(1.25 * SQUARESIZE)),
+    'Quit Game',
+    colorfamily=RED_FAMILY
+)
+
+alg_options = pyg_util.GameOptions(  # Opponent settings
+    (GLOBALBOARDSIZE + BOARDERSIZE, LOCALBOARDSIZE + WHITESPACE),
+    BEGINNER,  # default value
+    TWO_PLAYER,
+    BEGINNER,
+    FOO1,
+    FOO2,
+    FOO3
+)
+
+order_options = pyg_util.GameOptions(  # Turn order settings
+    (GLOBALBOARDSIZE + BOARDERSIZE + int(0.55 * LOCALBOARDSIZE), LOCALBOARDSIZE + WHITESPACE),
+    RANDOM_ORDER,  # default value
+    RANDOM_ORDER,
+    PLAYER_FIRST,
+    PLAYER_SECOND
+)
+
 """End PyGame Initialization"""
 
 
-def init_variables():
-    """Initializes the game variables to their defaults."""
-    global global_board, player, bot, bot_alg, game_over, reset
+class GlobalVariables:
+    def __init__(self) -> None:
+        self.global_board: GlobalBoard = GlobalBoard()
+        self.player: int = 1  # player will always be 1 or 2. 1 -> 'X' and 2 -> 'O'
+        self.bot_alg: str = alg_options.get_option()
+        order: str = order_options.get_option()
+        self.bot: int
 
-    # Global board
-    global_board = GlobalBoard()
+        # Decide whether the bot goes first or second
+        if self.bot_alg == TWO_PLAYER:
+            self.bot = 0
+        elif order == RANDOM_ORDER:
+            self.bot = rand.randint(1, 2)
+        elif order == PLAYER_FIRST:
+            self.bot = 2
+        elif order == PLAYER_SECOND:
+            self.bot = 1
 
-    player = 1  # player will always be 1 or 2. 1 -> 'X' and 2 -> 'O'
-
-    bot_alg = alg_options.get_option()
-    mode = order_options.get_option()
-
-    # Decide whether the bot goes first or second
-    if bot_alg == '2 Player':
-        bot = 0
-    elif mode == 'Random':
-        bot = rand.randint(1, 2)
-    elif mode == 'First':
-        bot = 2
-    elif mode == 'Second':
-        bot = 1
-
-    game_over = False  # break out of the game loop when the game ends
-    reset = False  # after the game, you are stuck in keep_alive() until reset == True
-
-    # global_board.print_board()  # command line
-    draw_board()  # GUI
-    draw_menu()  # GUI
-    pygame.display.flip()
+        self.game_over: bool = False  # break out of the game loop when the game ends
+        self.reset: bool = False  # after the game, you are stuck in keep_alive() until reset == True
 
 
-def draw_menu():
+GLOBALS = GlobalVariables()
+
+
+def draw_menu() -> None:
     """Draws the menu area on the right side of the GUI"""
     pygame.draw.rect(screen, DARK_GRAY, MENUAREA)
 
@@ -78,43 +116,43 @@ def draw_menu():
     order_options.draw(screen)
 
 
-def update_text():
+def update_text() -> None:
     """Updates the text displayed on the TextArea"""
-    if not game_over:
-        if player != bot:
-            if player == 1:
+    if not GLOBALS.game_over:
+        if GLOBALS.bot_alg != 'minimax' or GLOBALS.player != GLOBALS.bot:
+            if GLOBALS.player == 1:
                 textarea.set_text("Player X:", "Make your move", screen, color=BLUE)
             else:
                 textarea.set_text("Player O:", "Make your move", screen, color=RED)
     else:
-        if player == 0:
+        if GLOBALS.player == 0:
             textarea.set_text("The game is", "a draw.", screen)
-        elif player == 1:
+        elif GLOBALS.player == 1:
             textarea.set_text("Player X", "has won!", screen, color=BLUE)
         else:
             textarea.set_text("Player O", "has won!", screen, color=RED)
 
 
-def draw_board(update=True):
-    """Displays the full global and local boards in the GUI. Does not update the menu"""
+def draw_board(update: bool = True) -> None:
+    """Displays the full global and local boards in the GUI. Does not update the menu."""
     pygame.draw.rect(screen, LIGHT_GRAY, GLOBALBOARDAREA)
 
     # For each local board
     for outer_x in range(3):
         for outer_y in range(3):
             # Get the current local board
-            local_board = global_board.local_boards[outer_y * 3 + outer_x]
+            local_board = GLOBALS.global_board.local_board_list[outer_y * 3 + outer_x]
 
             # Top left coordinate of the current local board
             board_origin_x = BOARDERSIZE + ((LOCALBOARDSIZE + WHITESPACE) * outer_x)
             board_origin_y = BOARDERSIZE + ((LOCALBOARDSIZE + WHITESPACE) * outer_y)
 
             # Color the board accordingly if it is won by X or O, or if it is in focus
-            if local_board.focus and not game_over:  # if the game is over, nothing is in focus
+            if local_board.focus and not GLOBALS.game_over:  # if the game is over, nothing is in focus
                 pygame.draw.rect(screen, WHITE, (board_origin_x, board_origin_y, LOCALBOARDSIZE, LOCALBOARDSIZE))
-            elif global_board.board[outer_y][outer_x] == 1:
+            elif GLOBALS.global_board.board[outer_y][outer_x] == 1:
                 pygame.draw.rect(screen, LIGHT_BLUE, (board_origin_x, board_origin_y, LOCALBOARDSIZE, LOCALBOARDSIZE))
-            elif global_board.board[outer_y][outer_x] == 2:
+            elif GLOBALS.global_board.board[outer_y][outer_x] == 2:
                 pygame.draw.rect(screen, LIGHT_RED, (board_origin_x, board_origin_y, LOCALBOARDSIZE, LOCALBOARDSIZE))
 
             # Draw the grid lines for the local board
@@ -147,28 +185,11 @@ def draw_board(update=True):
         pygame.display.update(GLOBALBOARDAREA)
 
 
-# def get_lb_name(local_board):
-#     # Returns the physical location of the local board given lb as a key (for the command line)
-#     switcher = {
-#         0: "top left",
-#         1: "top center",
-#         2: "top right",
-#         3: "center left",
-#         4: "center board",
-#         5: "center right",
-#         6: "bottom left",
-#         7: "bottom center",
-#         8: "bottom right"
-#     }
-#     return switcher.get(local_board.index)
-
-
-def get_inputs():
+def get_inputs(mouse: Tuple[int, int]) -> Optional[Tuple[LocalBoard, int, int]]:
     """Gets the current position of the mouse and returns the local board, as well as row and column coordinates of the
     square that the mouse is currently in. If the mouse is not in a square, then local_board will return None."""
 
-    x_pos, y_pos = pygame.mouse.get_pos()  # current x and y coordinates of the mouse
-    local_board = None  # default value for local_board
+    x_pos, y_pos = mouse  # current x and y coordinates of the mouse
 
     # For each local board
     for x in range(3):
@@ -177,114 +198,69 @@ def get_inputs():
             board_origin_x = BOARDERSIZE + ((LOCALBOARDSIZE + WHITESPACE) * x)
             board_origin_y = BOARDERSIZE + ((LOCALBOARDSIZE + WHITESPACE) * y)
 
-            # If the mouse is over one of the boards, set local_board accordingly
-            if board_origin_x < x_pos < board_origin_x + LOCALBOARDSIZE and \
-                    board_origin_y < y_pos < board_origin_y + LOCALBOARDSIZE:
-                local_board = global_board.local_boards[y * 3 + x]
-                break
-        # How to break out of nested for loops in python:
-        else:  # if the inner loop does not break, continue
-            continue
-        break  # if the inner loop does break, then break out of the outer loop too
+            # If the mouse is over the current local_board area, return local_board, row, and col
+            if 0 < x_pos - board_origin_x < LOCALBOARDSIZE and 0 < y_pos - board_origin_y < LOCALBOARDSIZE:
+                local_board = GLOBALS.global_board.local_board_list[y * 3 + x]
+                row = (y_pos - board_origin_y) // SQUARESIZE
+                col = (x_pos - board_origin_x) // SQUARESIZE
+                return local_board, row, col
 
-    # row and col will be nonsense if the mouse is not over a board. Need to ensure local_board is not None before using
-    row_pos = (y_pos - board_origin_y) // SQUARESIZE
-    col_pos = (x_pos - board_origin_x) // SQUARESIZE
-
-    return local_board, row_pos, col_pos
+    # If the mouse was not over a local board, return None
+    return None
 
 
-# def get_next_move():
-#     # Request local board and row and col coordinates from the player (for the command line)
-#     # DO NOT RUN WHILE USING THE GUI
-#     while True:
-#         next_lb_index = int(input(f"Player {player}, please select your local board (1-9): ")) - 1
-#         next_lb = board.local_boards[next_lb_index]
-#         if next_lb.focus:
-#             print(f"The {get_lb_name(next_lb.index)} board is in focus.")
-#             next_row = int(input(f"Player {player}, please enter the row (1-3): ")) - 1
-#             next_col = int(input(f"Player {player}, please enter the column (1-3): ")) - 1
-#
-#             if next_lb.board[next_row][next_col] == 0:
-#                 return next_lb, next_row, next_col
-#             else:
-#                 print("That space has already been played.")
-#         else:
-#             print(f"The {get_lb_name(next_lb.index)} board is not playable.")
+def make_move(local_board: LocalBoard, row: int, col: int) -> None:
+    """Takes a local board and the coordinates of a space on the board, and marks the space for the current player. Then
+    checks for global board updates and acts accordingly."""
 
-
-def update_focus(old_row, old_col):
-    """Use the previous move to set the focus of the local boards for the next turn"""
-
-    # Local board in the same position as the previous guess. May or may not be playable
-    next_lb = global_board.local_boards[old_row * 3 + old_col]
-
-    # if the board is playable, set focus to True, and all others to False
-    if next_lb.playable:
-        for local_board in global_board.local_boards:
-            local_board.focus = False
-        next_lb.focus = True
-    # if the board is not playable, set all playable boards in focus, and all non-playable boards out of focus
-    else:
-        for local_board in global_board.local_boards:
-            local_board.focus = local_board.playable
-
-
-def make_move(local_board, row_pos, col_pos):
-    """Takes a local board and the coordinates of a space on the board, and marks the space for the current player"""
-
-    global player, game_over
-
-    local_board.board[row_pos][col_pos] = player  # set space to player
+    local_board.board[row][col] = GLOBALS.player  # set space to player
 
     # Check if this move determines the outcome of the local board (win, lose, draw)
-    if local_board.has_tic_tac_toe(player):
+    if local_board.has_tic_tac_toe(GLOBALS.player):
         # if local board has been won, set playable to False, then mark the global board
         local_board.playable = False
-        global_board.mark_global_board(local_board, player)
+        GLOBALS.global_board.mark_global_board(local_board, GLOBALS.player)
 
         # Now check if this determines the outcome of the global board. If so, the game is over
-        if global_board.has_tic_tac_toe(player):
-            game_over = True
-        elif global_board.is_full():
-            game_over = True
-            player = 0
+        if GLOBALS.global_board.has_tic_tac_toe(GLOBALS.player):
+            GLOBALS.game_over = True
+        elif GLOBALS.global_board.is_full():
+            GLOBALS.game_over = True
+            GLOBALS.player = 0
 
     # if the local board is a draw
     elif local_board.is_full():
         local_board.playable = False
-        global_board.mark_global_board(local_board, -1)
+        GLOBALS.global_board.mark_global_board(local_board, -1)
 
-        if global_board.is_full():
-            game_over = True
+        if GLOBALS.global_board.is_full():
+            GLOBALS.game_over = True
 
     # update the focus of the local boards for the next turn
-    update_focus(row_pos, col_pos)
-
-    # global_board.print_board()  # Command Line
-    draw_board()  # GUI
+    GLOBALS.global_board.update_focus(row, col)
 
     # switch player 1 <-> 2
-    if not game_over:
-        player = (player % 2) + 1
+    if not GLOBALS.game_over:
+        GLOBALS.player = (GLOBALS.player % 2) + 1
 
+    # GLOBALS.global_board.print_board()  # Print board to standard out
+    draw_board()  # Draw board in GUI
     update_text()
 
 
-def main():
+def main() -> None:
     """The main game loop. Initializes the global variables, then plays one game of ultimate tic tac toe"""
-    global reset
 
-    init_variables()
+    while not GLOBALS.game_over:
+        if GLOBALS.player == GLOBALS.bot:  # bot turn
+            if GLOBALS.bot_alg == BEGINNER:
+                lb, row, col = minimax.bot_turn(GLOBALS.global_board, GLOBALS.bot)  # get the bot's move
+            else:
+                raise Exception("Undefined bot algorithm")
 
-    # Game loop
-    while not game_over:
-        # Bot turn
-        if player == bot:
-            if bot_alg == 'Very Easy':
-                lb, row, col = minimax.bot_turn(global_board, bot)  # get the bot's move
             make_move(lb, row, col)  # record the move and update the GUI
-        else:
+
+        else:  # human turn (will always be the case in 2-player mode
             for event in pygame.event.get():
                 mouse = pygame.mouse.get_pos()
 
@@ -292,7 +268,7 @@ def main():
                 if newgamebutton.is_button_event(event, mouse):
                     newgamebutton.draw(screen)
                     if event.type == pygame.MOUSEBUTTONUP:
-                        reset = True
+                        GLOBALS.reset = True
                         return
 
                 # Show Rules Button
@@ -316,14 +292,16 @@ def main():
                     sys.exit()
 
                 elif alg_options.is_event(event, mouse, screen) or order_options.is_event(event, mouse, screen):
+                    # is_event handles drawing any affected options itself, so no need to call a draw method here
                     pass
 
                 # Human Turn
                 elif event.type == pygame.MOUSEBUTTONUP:
                     # Get lb, and row and col coordinates from get_inputs(). Check if lb is None
-                    lb, row, col = get_inputs()
-                    if lb is not None:
-                        # Check if selected space has already been played
+                    params = get_inputs(mouse)
+                    if params is not None:
+                        lb, row, col = params
+                        # Check if local board is in focus and if selected space has not yet been played
                         if lb.focus and lb.board[row][col] == 0:
                             make_move(lb, row, col)
 
@@ -331,7 +309,7 @@ def main():
                 # depending on the turn
                 elif event.type == pygame.MOUSEMOTION and mouse[0] < GLOBALBOARDSIZE - int(1.1 * DIFF):
                     # draw an 'X' or 'O' where the mouse is pointed
-                    if player == 1:
+                    if GLOBALS.player == 1:
                         draw_x(mouse, screen)
                     else:
                         pygame.draw.circle(screen, RED, mouse, DIFF, 4)
@@ -344,10 +322,10 @@ def main():
                     draw_board(False)
 
 
-def keep_alive():
+def keep_alive() -> None:
     """Keep the screen up after the game ends until the user quits or resets"""
-    global reset
-    while not reset:
+    # global reset
+    while not GLOBALS.reset:
         for event in pygame.event.get():
             mouse = pygame.mouse.get_pos()
 
@@ -355,7 +333,7 @@ def keep_alive():
             if newgamebutton.is_button_event(event, mouse):
                 newgamebutton.draw(screen)
                 if event.type == pygame.MOUSEBUTTONUP:
-                    reset = True
+                    GLOBALS.reset = True
                     return
 
             # Show Rules Button
@@ -386,6 +364,13 @@ if __name__ == '__main__':
     """After main(), the program will be stuck in keep_alive() until the game is reset or quit. If the user quits,
     sys.exit() is called and the program will quit. If the user resets, the program will simply exit keep_alive(), and
     the loop will start over."""
+
     while True:
+        # GLOBALS.global_board.print_board()  # command line
+        draw_board(update=False)  # GUI
+        draw_menu()  # GUI
+        pygame.display.flip()
+
         main()
         keep_alive()
+        GLOBALS = GlobalVariables()
